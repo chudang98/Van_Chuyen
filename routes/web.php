40ce78d73d28962abb03fed08bbb07e6b00ff648
N.P Thao <nguyenphuongthao12041998@gmail.com>
2019-08-07 11:21:06 +0700

    cap nhat

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
 })->name('intro');
 
 Auth::routes();
-Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
 Route::get('/home', 'HomeController@index')->name('home');
 
 
