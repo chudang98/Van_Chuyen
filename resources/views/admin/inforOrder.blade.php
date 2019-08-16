3da1714a12a3a445b2a170003d81099e406a6ebb
Duck <chudang98hn@gmail.com>
2019-08-16 15:39:17 +0700

    init order detail

--- resources/views/admin/inforOrder.blade.php
+++ resources/views/admin/inforOrder.blade.php
@@ -15,7 +15,7 @@
                 Thông tin hóa đơn
             </h2>
             <div class="form-row top-mar">
-                <label class="col-md-3" for="sender_name">Sender name</label>
+                <label class="col-md-3" >Sender name</label>
                 <div class="input-group col-md-5">
                     <div class="input-group-addon">
                         <i class="fas fa-user-circle"></i>
@@ -24,16 +24,16 @@
                 </div>
             </div>
             <div class="form-row top-mar">
-                <label class="col-md-3" for="sender_name">Sender telephone</label>
+                <label class="col-md-3" >Sender telephone</label>
                 <div class="input-group col-md-5">
                     <div class="input-group-addon">
-                        <i class="fas fa-pencil-alt"></i>
+                        <i class="fas fa-phone"></i>
                     </div>
-                    <input class="form-control" readonly value="{{ $user['phone_client'] }}"/>
+                    <input class="form-control" readonly value="{{ $data['phone_client'] }}"/>
                 </div>
             </div>
             <div class="form-row top-mar">
-                <label class="col-md-3" for="sender_name">Receiver name</label>
+                <label class="col-md-3" >Receiver name</label>
                 <div class="input-group col-md-5">
                     <div class="input-group-addon">
                         <i class="fas fa-pencil-alt"></i>
@@ -42,7 +42,16 @@
                 </div>
             </div>
             <div class="form-row top-mar">
-                <label class="col-md-3" for="sender_name">Sender name</label>
+                <label class="col-md-3" >Receiver telephone</label>
+                <div class="input-group col-md-5">
+                    <div class="input-group-addon">
+                        <i class="fas fa-phone-volume"></i>
+                    </div>
+                    <input class="form-control" readonly value="{{ $data['phone_reciever'] }}"/>
+                </div>
+            </div>
+            <div class="form-row top-mar">
+                <label class="col-md-3" ></label>
                 <div class="input-group col-md-5">
                     <div class="input-group-addon">
                         <i class="fas fa-pencil-alt"></i>
@@ -51,7 +60,7 @@
                 </div>
             </div>
             <div class="form-row top-mar">
-                <label class="col-md-3" for="sender_name">Sender name</label>
+                <label class="col-md-3" ></label>
                 <div class="input-group col-md-5">
                     <div class="input-group-addon">
                         <i class="fas fa-pencil-alt"></i>
@@ -59,7 +68,42 @@
                     <input class="form-control" readonly value=""/>
                 </div>
             </div>
-
+            <div class="form-row top-mar">
+                <label class="col-md-3" ></label>
+                <div class="input-group col-md-5">
+                    <div class="input-group-addon">
+                        <i class="fas fa-pencil-alt"></i>
+                    </div>
+                    <input class="form-control" readonly value=""/>
+                </div>
+            </div>
+            <div class="form-row top-mar">
+                <label class="col-md-3" ></label>
+                <div class="input-group col-md-5">
+                    <div class="input-group-addon">
+                        <i class="fas fa-pencil-alt"></i>
+                    </div>
+                    <input class="form-control" readonly value=""/>
+                </div>
+            </div>
+            <div class="form-row top-mar">
+                <label class="col-md-3" ></label>
+                <div class="input-group col-md-5">
+                    <div class="input-group-addon">
+                        <i class="fas fa-pencil-alt"></i>
+                    </div>
+                    <input class="form-control" readonly value=""/>
+                </div>
+            </div>
+            <div class="form-row top-mar">
+                <label class="col-md-3"></label>
+                <div class="input-group col-md-5">
+                    <div class="input-group-addon">
+                        <i class="fas fa-pencil-alt"></i>
+                    </div>
+                    <input class="form-control" readonly value=""/>
+                </div>
+            </div>
         </div>
 
     </div>
