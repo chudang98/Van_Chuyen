af4230a77d26e03396c2fec3869b48ee5b0af9ba
Duck <chudang98hn@gmail.com>
2019-08-16 16:15:56 +0700

    done order detail

--- resources/views/admin/inforOrder.blade.php
+++ resources/views/admin/inforOrder.blade.php
@@ -12,7 +12,7 @@
     <div class="container text-center">
         <div class="row">
             <h2 class="text-center">
-                Thông tin hóa đơn
+                Thông tin hóa đơn {{ $data['id'] }}
             </h2>
             <div class="form-row top-mar">
                 <label class="col-md-3" >Sender name</label>
@@ -33,10 +33,19 @@
                 </div>
             </div>
             <div class="form-row top-mar">
+                    <label class="col-md-3">Sender address</label>
+                    <div class="input-group col-md-5">
+                        <div class="input-group-addon">
+                            <i class="fas fa-map-marked-alt"></i>
+                        </div>
+                        <input class="form-control" readonly value="{{ $data['address_client'] }}"/>
+                    </div>
+                </div>
+            <div class="form-row top-mar">
                 <label class="col-md-3" >Receiver name</label>
                 <div class="input-group col-md-5">
                     <div class="input-group-addon">
-                        <i class="fas fa-pencil-alt"></i>
+                        <i class="fas fa-user-check"></i>
                     </div>
                     <input class="form-control" readonly value="{{ $data['name_reciever'] }}"/>
                 </div>
@@ -50,61 +59,78 @@
                     <input class="form-control" readonly value="{{ $data['phone_reciever'] }}"/>
                 </div>
             </div>
+
             <div class="form-row top-mar">
-                <label class="col-md-3" ></label>
+                <label class="col-md-3">Recriver address</label>
                 <div class="input-group col-md-5">
                     <div class="input-group-addon">
-                        <i class="fas fa-pencil-alt"></i>
+                        <i class="fas fa-map-marker-alt"></i>
                     </div>
-                    <input class="form-control" readonly value=""/>
+                    <input class="form-control" readonly value="{{ $data['address_reciever'] }}"/>
                 </div>
             </div>
             <div class="form-row top-mar">
-                <label class="col-md-3" ></label>
+                <label class="col-md-3" >Date order</label>
                 <div class="input-group col-md-5">
                     <div class="input-group-addon">
-                        <i class="fas fa-pencil-alt"></i>
+                        <i class="fas fa-calendar-alt"></i>
                     </div>
-                    <input class="form-control" readonly value=""/>
+                    <input class="form-control" readonly value="{{ $data['start_date'] }}"/>
                 </div>
             </div>
             <div class="form-row top-mar">
-                <label class="col-md-3" ></label>
+                <label class="col-md-3">Price order</label>
                 <div class="input-group col-md-5">
                     <div class="input-group-addon">
-                        <i class="fas fa-pencil-alt"></i>
+                        <i class="fas fa-dollar-sign"></i>
                     </div>
-                    <input class="form-control" readonly value=""/>
+                    <input id='price' class="form-control" readonly value="{{ $data['total_price'] }} VNĐ"/>
                 </div>
             </div>
             <div class="form-row top-mar">
-                <label class="col-md-3" ></label>
+                <label class="col-md-3" >Payment</label>
                 <div class="input-group col-md-5">
                     <div class="input-group-addon">
-                        <i class="fas fa-pencil-alt"></i>
+                        <i class="fas fa-credit-card"></i>
                     </div>
-                    <input class="form-control" readonly value=""/>
+                    <input class="form-control" readonly value="{{ $data['payment'] }}"/>
                 </div>
             </div>
             <div class="form-row top-mar">
-                <label class="col-md-3" ></label>
+                <label class="col-md-3">Speed</label>
                 <div class="input-group col-md-5">
                     <div class="input-group-addon">
-                        <i class="fas fa-pencil-alt"></i>
+                        <i class="fas fa-truck-moving"></i>
                     </div>
-                    <input class="form-control" readonly value=""/>
-                </div>
-            </div>
-            <div class="form-row top-mar">
-                <label class="col-md-3"></label>
-                <div class="input-group col-md-5">
-                    <div class="input-group-addon">
-                        <i class="fas fa-pencil-alt"></i>
-                    </div>
-                    <input class="form-control" readonly value=""/>
+                    <input class="form-control" readonly value="{{ $data['speed']}}"/>
                 </div>
             </div>
         </div>
 
+        <div class="row justify-content-md-center">
+            <div class="col-md-4">
+                <a href="/orders">
+                    <button class="btn btn-primary" type="button">
+                    Back</button>
+                </a>
+            </div>
+        </div>
+
+
     </div>
+@endsection
+
+@section('script')
+    <script>
+        $(document).ready(function(){
+            var price = {{ $data['total_price'] }},
+                price_formated = formatNumber(price);
+            $('#price').val(price_formated + " VNĐ");
+        });
+
+        function formatNumber(num){
+            return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+        }
+    </script>
+
 @endsection
